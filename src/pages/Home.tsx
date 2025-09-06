00ae53cbaa20f272780404a40b2b49bf3fa5447b
Sforsuraj <surajraj.9626@gmail.com>
2025-09-06 15:45:42 +0530

    update to main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -71,7 +71,6 @@ const Home: React.FC = () => {
       {/* Events */}
       <section id="events" className="events-container">
         <h2>Technical Events</h2>
-        <Link to={`/registersheet`}>Click me</Link>
         <div className="cards">
           {technicalEvents.map((event) => (
             <Link key={event.id} to={`/event/${event.id}`}>
