57ba860ba7f8f02f660be9d820af85bf9ebfb716
Sforsuraj <surajraj.9626@gmail.com>
2025-09-08 11:38:52 +0530

    update event

--- src/data/eventsdescription.ts
+++ src/data/eventsdescription.ts
@@ -1,6 +1,6 @@
 export const allEvents = [
   {
-    id: "1",
+    id: "3",
     title: "CODER DHINAM: A Day of Logic, Teamwork, and Clean Code",
     desc: "A competitive event focusing on problem-solving, code efficiency, and teamwork.",
     sections: [
@@ -79,6 +79,46 @@ export const allEvents = [
           {
             subheading: "Round 1 – Online",
             content: "Round 1 is an online Tamil movie musical quiz with MCQs on 90s-2k films, actors, and iconic dialogues. Top scorers advance, celebrating nostalgia through interactive digital fun.",
+          },
+          {
+            subheading: "Round 2",
+            content: "In this round, participants will see photos of directors, heroes, and heroines from iconic 90s and 2k Tamil movies. Their task is to identify the movies associated with these images, testing both their visual memory and cinematic knowledge in a thrilling, competitive format.",
+          },
+          {
+            subheading: "Round 3",
+            content: "In this round, a background music (BGM) clip from a classic 90s or 2k Tamil movie will be played. Participants must identify the music director. Along with the BGM, a movie picture will be shown, prompting questions about the movie’s director, hero, heroine, or the movie title itself.",
+          },
+          {
+            subheading: "Round 3 - Final",
+            content: "In the thrilling final round, one teammate from each team acts out movie titles conveyed through Dumb Charades, while others guess within a time limit. Alongside this, teams tackle a 'Connection Game,' deciphering links between pictures or clues tied to a single movie or song. Adding to the excitement, teams can use creative sabotages to challenge opponents",
+          },
+        ],
+      },
+      {
+        heading: "Bonus Fun",
+        content: "Expect surprising musical cues, cinematic twists, and a joyous celebration of 90s–2k movies and melodies throughout the event.",
+      },
+    ],
+  },
+  {
+  id: "1",
+    title: "DATA AVATHARAM",
+    desc: "A competitive event focusing on problem-solving, code efficiency, and teamwork.",
+    sections: [
+      {
+        heading: "Team Dynamics",
+        content: "Two brains, two systems, one mission—decode data like never before! Each team of two will split their skills across quiz mastery, debugging sharpness, and dashboard creativity. Teamwork, time management, and analytical thinking will be your strongest allies.",
+      },
+      {
+        heading: "Event Structure",
+        subsections: [
+          {
+            subheading: "",
+            content: "The competition unfolds in two thrilling rounds, each designed to test different facets of data analytics:",
+          },
+          {
+            subheading: "Round 1 – Tag Team Challenge",
+            content: "•	Each team will have two systems.\n• Member 1 kicks off with a quiz on the first system. The clock starts ticking here!\n•	Once the quiz is completed, Member 2 takes over on the second system for a debugging challenge.\n•	The faster and sharper your debugging, the higher your score climbs.\n•	Winning criteria: Number of debugging problems solved within the given time.",
           },
           {
             subheading: "Round 2",
